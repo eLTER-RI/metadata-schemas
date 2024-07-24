31b6aeae1e5f166f8d293c1f07f70c117c85fbb5
Dominik Dubovsky <485020@muni.cz>
2024-07-24 16:55:46 +0200

    Search page changes

--- ui/lter/semantic-ui/js/lter/search/components/index.js
+++ ui/lter/semantic-ui/js/lter/search/components/index.js
@@ -1,1 +1,7 @@
 export * from "./facets";
+export * from "./EmptyResultsElement";
+export * from "./SearchBar"
+export * from "./ResultCount"
+export * from "./DoubleSeparator"
+export * from "./SearchFacetLink"
+export * from "./MultipleSearchBarElement"
