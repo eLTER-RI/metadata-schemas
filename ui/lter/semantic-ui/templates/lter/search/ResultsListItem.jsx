f50ecf874c76cd3f86dac49de6ea0e53f085ba29
Dominik Dubovsky <485020@muni.cz>
2024-07-24 14:16:32 +0200

    Impl of search list item

--- ui/lter/semantic-ui/templates/lter/search/ResultsListItem.jsx
+++ ui/lter/semantic-ui/templates/lter/search/ResultsListItem.jsx
@@ -1,123 +1,122 @@
-import React, { useContext } from "react";
+import React, {useContext} from "react";
 import PropTypes from "prop-types";
 import Overridable from "react-overridable";
 
 import _get from "lodash/get";
 
-import { Grid, Item } from "semantic-ui-react";
-import { withState, buildUID } from "react-searchkit";
-import { SearchConfigurationContext } from "@js/invenio_search_ui/components";
+import {Grid, Item} from "semantic-ui-react";
+import {withState, buildUID} from "react-searchkit";
+import {SearchConfigurationContext} from "@js/invenio_search_ui/components";
 
-import { i18next } from "@translations/i18next";
+import {i18next} from "@translations/i18next";
 
-const ItemHeader = ({titles, viewLink }) => {
-  return (
-    <Item.Header as="h2">
-      <a href={viewLink}>{titles[0].text}</a>
-    </Item.Header>
-  );
+const ItemHeader = ({titles, viewLink}) => {
+    return (
+        <Item.Header as="h2">
+            <a href={viewLink}>{titles[0].text}</a>
+        </Item.Header>
+    );
 };
 
 const ItemSubheader = ({}) => {
-  // just an example
-  return (
-    <>
-      <Item.Meta>
-        <Grid columns={1}>
-          <Grid.Column>
-            <Grid.Row className="ui double separated creatibutors"></Grid.Row>
-          </Grid.Column>
-        </Grid>
-      </Item.Meta>
-    </>
-  );
+    // just an example
+    return (
+        <>
+            <Item.Meta>
+                <Grid columns={1}>
+                    <Grid.Column>
+                        <Grid.Row className="ui double separated creatibutors">Something here</Grid.Row>
+                    </Grid.Column>
+                </Grid>
+            </Item.Meta>
+        </>
+    );
 };
 
 export const ResultsListItemComponent = ({
-  currentQueryState,
-  result,
-  appName,
-  ...rest
-}) => {
-  const searchAppConfig = useContext(SearchConfigurationContext);
+                                             currentQueryState,
+                                             result,
+                                             appName,
+                                             ...rest
+                                         }) => {
+    const searchAppConfig = useContext(SearchConfigurationContext);
 
-  const titles = _get(result, "metadata.titles", [{"text": "No Title"}]);
+    const titles = _get(result, "metadata.titles", [{"text": "No Title"}]);
 
-  return (
-    <Overridable
-      id={buildUID("RecordsResultsListItem.layout", "", appName)}
-      result={result}
-      titles={titles}
-      {...rest}
-    >
-      <Item key={result.id}>
-        <Item.Content>
-          <Grid>
-            <Grid.Row columns={1}>
-              <Grid.Column className="results-list item-main">
-                <ItemHeader
-                  titles={titles}
-                  viewLink={result.links.self_html}
-                />
-                <ItemSubheader />
-                <Item.Description />
-              </Grid.Column>
-            </Grid.Row>
-          </Grid>
-        </Item.Content>
-      </Item>
-    </Overridable>
-  );
+    return (
+        <Overridable
+            id={buildUID("RecordsResultsListItem.layout", "", appName)}
+            result={result}
+            titles={titles}
+            {...rest}
+        >
+            <Item key={result.id} className="search-listing-item" href={result.links.self_html}>
+                <Item.Content className="content">
+                    <Grid>
+                        <Grid.Row columns={1}>
+                            <Grid.Column className="results-list item-main">
+                                <ItemHeader
+                                    titles={titles}
+                                    viewLink={result.links.self_html}
+                                />
+                                <ItemSubheader/>
+                            </Grid.Column>
+                        </Grid.Row>
+                    </Grid>
+                </Item.Content>
+            </Item>
+        </Overridable>
+    );
 };
 
 ResultsListItemComponent.propTypes = {
-  currentQueryState: PropTypes.object,
-  result: PropTypes.object.isRequired,
-  appName: PropTypes.string,
+    currentQueryState: PropTypes.object,
+    result: PropTypes.object.isRequired,
+    appName: PropTypes.string,
 };
 
 ResultsListItemComponent.defaultProps = {
-  currentQueryState: null,
-  appName: "",
+    currentQueryState: null,
+    appName: "",
 };
 
 export const ResultsListItem = (props) => {
-  return (
-    <Overridable id={buildUID("ResultsListItem", "", props.appName)} {...props}>
-      <ResultsListItemComponent {...props} />
-    </Overridable>
-  );
+    return (
+        <Overridable id={buildUID("ResultsListItem", "", props.appName)} {...props}>
+            <ResultsListItemComponent {...props} />
+        </Overridable>
+    );
 };
 
 ResultsListItem.propTypes = {
-  currentQueryState: PropTypes.object,
-  result: PropTypes.object.isRequired,
-  appName: PropTypes.string,
+    currentQueryState: PropTypes.object,
+    result: PropTypes.object.isRequired,
+    appName: PropTypes.string,
 };
 
 ResultsListItem.defaultProps = {
-  currentQueryState: null,
-  appName: "",
+    currentQueryState: null,
+    appName: "",
 };
 
 export const ResultsListItemWithState = withState(
-  ({ currentQueryState, updateQueryState, result, appName }) => (
-    <ResultsListItem
-      currentQueryState={currentQueryState}
-      updateQueryState={updateQueryState}
-      result={result}
-      appName={appName}
-    />
-  )
+    ({currentQueryState, updateQueryState, result, appName}) => (
+        <ResultsListItem
+            currentQueryState={currentQueryState}
+            updateQueryState={updateQueryState}
+            result={result}
+            appName={appName}
+        />
+    )
 );
 
 ResultsListItemWithState.propTypes = {
-  currentQueryState: PropTypes.object,
-  result: PropTypes.object.isRequired,
+    currentQueryState: PropTypes.object,
+    result: PropTypes.object.isRequired,
 };
 
 ResultsListItemWithState.defaultProps = {
-  currentQueryState: null,
+    currentQueryState: null,
 };
 
 export default ResultsListItemWithState;
