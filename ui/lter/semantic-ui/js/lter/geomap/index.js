5b889f085e5d0fe65bb7e3f57b77f19b37dd0900
Dominik Dubovsky <485020@muni.cz>
2024-08-02 15:30:15 +0200

    Refactor

--- ui/lter/semantic-ui/js/lter/geomap/index.js
+++ ui/lter/semantic-ui/js/lter/geomap/index.js
@@ -4,7 +4,7 @@ import ReactDOM from "react-dom";
 import {GeoLocationMap} from "./components";
 
 const geoMapElement = document.getElementById("geo-map");
-const geoLocationsRawString = geoMapElement.getAttribute('geoData');
+const geoLocationsRawString = geoMapElement.getAttribute('data-geo');
 const geoLocationsJsonString = geoLocationsRawString.replace(/\\u0027/g, "\"").slice(1, -1);
 const geoLocationsObject = JSON.parse(geoLocationsJsonString)
 
