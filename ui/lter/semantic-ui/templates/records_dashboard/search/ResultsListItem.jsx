d502d5439f29e6c8e04b9448693fba2bf45cf75e
Dominik Dubovsky <485020@mail.muni.cz>
2024-11-04 08:57:10 +0100

    Refactor - change publish button location

--- ui/lter/semantic-ui/templates/records_dashboard/search/ResultsListItem.jsx
+++ ui/lter/semantic-ui/templates/records_dashboard/search/ResultsListItem.jsx
@@ -8,7 +8,7 @@ import {Grid, Item, Label, Icon, Button, Modal, Loader, Popup} from "semantic-ui
 import {withState, buildUID} from "react-searchkit";
 import {SearchConfigurationContext} from "@js/invenio_search_ui/components";
 import axios from "axios";
-import {DeleteButton} from "./components/ActionButtons";
+import {DeleteButton, PublishButton} from "./components/ActionButtons";
 
 
 // import {i18next} from "@translations/i18next";
@@ -167,70 +167,6 @@ ResultsListItemWithState.propTypes = {
 ResultsListItemWithState.defaultProps = {
     currentQueryState: null,
 };
-
-const PublishButton = ({record}) => {
-    const [open, setOpen] = useState(false); // State to control modal visibility
-    const [loading, setLoading] = useState(false); // State to control button loading state
-
-    const draftId = _get(record, "id", "No-Id")
-
-    const handlePublish = async () => {
-        setLoading(true);
-        try {
-            const requestInfo = await axios.post(`/api/lter/${draftId}/draft/requests/publish_draft`)
-            const publishRequestId = _get(requestInfo.data, "id", "No Link")
-            await axios.post(`/api/requests/${publishRequestId}/actions/submit`)
-            setOpen(false);
-        } catch (error) {
-            console.error("Error publishing:", error);
-        } finally {
-            setLoading(false);
-            location.reload();
-        }
-    };
-
-    const handlePublishButtonClick = (event) => {
-        event.preventDefault();
-        setOpen(true)
-    }
-
-    return (
-        <div>
-            <Button fluid secondary onClick={(e) => handlePublishButtonClick(e)}>Publish</Button>
-
-            <Modal
-                open={open}
-                onClose={() => setOpen(false)}
-                size="small"
-            >
-                <Modal.Header>Confirm Publish</Modal.Header>
-                <Modal.Content>
-                    <p>Are you sure you want to publish the draft ?</p>
-                </Modal.Content>
-                <Modal.Actions>
-                    <Button onClick={() => setOpen(false)} disabled={loading}>
-                        Cancel
-                    </Button>
-                    <Button
-                        color="green"
-                        onClick={() => handlePublish(draftId)}
-                        loading={loading}
-                        disabled={loading}
-                    >
-                        Confirm Publish
-                    </Button>
-                </Modal.Actions>
-            </Modal>
-        </div>
-    );
-};
-
-// Define PropTypes
-PublishButton.propTypes = {
-    record: PropTypes.object.required,
-};
-
-
 
 
 export default ResultsListItemWithState;
--- ui/lter/semantic-ui/templates/records_dashboard/search/components/ActionButtons.jsx
+++ ui/lter/semantic-ui/templates/records_dashboard/search/components/ActionButtons.jsx
@@ -62,3 +62,65 @@ export const DeleteButton = ({record}) => {
 DeleteButton.propTypes = {
     record: PropTypes.object.required,
 };
+
+export const PublishButton = ({record}) => {
+    const [open, setOpen] = useState(false); // State to control modal visibility
+    const [loading, setLoading] = useState(false); // State to control button loading state
+
+    const draftId = _get(record, "id", "No-Id")
+
+    const handlePublish = async () => {
+        setLoading(true);
+        try {
+            const requestInfo = await axios.post(`/api/lter/${draftId}/draft/requests/publish_draft`)
+            const publishRequestId = _get(requestInfo.data, "id", "No Link")
+            await axios.post(`/api/requests/${publishRequestId}/actions/submit`)
+            setOpen(false);
+        } catch (error) {
+            console.error("Error publishing:", error);
+        } finally {
+            setLoading(false);
+            location.reload();
+        }
+    };
+
+    const handlePublishButtonClick = (event) => {
+        event.preventDefault();
+        setOpen(true)
+    }
+
+    return (
+        <div>
+            <Button fluid secondary onClick={(e) => handlePublishButtonClick(e)}>Publish</Button>
+
+            <Modal
+                open={open}
+                onClose={() => setOpen(false)}
+                size="small"
+            >
+                <Modal.Header>Confirm Publish</Modal.Header>
+                <Modal.Content>
+                    <p>Are you sure you want to publish the draft ?</p>
+                </Modal.Content>
+                <Modal.Actions>
+                    <Button onClick={() => setOpen(false)} disabled={loading}>
+                        Cancel
+                    </Button>
+                    <Button
+                        color="green"
+                        onClick={() => handlePublish(draftId)}
+                        loading={loading}
+                        disabled={loading}
+                    >
+                        Confirm Publish
+                    </Button>
+                </Modal.Actions>
+            </Modal>
+        </div>
+    );
+};
+
+// Define PropTypes
+PublishButton.propTypes = {
+    record: PropTypes.object.required,
+};
