c1ba1a18b1ce4fec88df9265dc5d4e7272a37d4f
Dominik Dubovsky <485020@mail.muni.cz>
2024-11-27 12:27:31 +0100

    Fix is_admin value passed to the react component

--- ui/lter/semantic-ui/js/lter/actionmenu/index.js
+++ ui/lter/semantic-ui/js/lter/actionmenu/index.js
@@ -6,12 +6,12 @@ import {ActionMenu} from "./ActionMenu";
 const actionMenu = document.getElementById("action-menu");
 const draftId = actionMenu.getAttribute('draftId');
 const state = actionMenu.getAttribute('state');
-const isAdmin = actionMenu.getAttribute('isAdmin')
+const isAdmin = actionMenu.getAttribute('isAdmin') === 'true'
 
 ReactDOM.render(
     <ActionMenu
         draftId={draftId}
         state={state}
-        isAdmin
+        isAdmin={isAdmin}
     />, actionMenu
 );
