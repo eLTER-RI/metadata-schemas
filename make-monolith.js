b63c5cfb817cd2470feea824e611b2a33a911891
1O <2448566+1O@users.noreply.github.com>
2025-07-24 15:09:23 +0200

    Update make-monolith.js

--- make-monolith.js
+++ make-monolith.js
@@ -1,6 +1,6 @@
-const { default: $RefParser } = await import("@apidevtools/json-schema-ref-parser");
-const fs = await import("fs")
-
+const { default: $RefParser } = require("@apidevtools/json-schema-ref-parser");
+const fs = require("fs")
+const path = require("path");
 
 // dereference these files by inlining referenced content (subschemas)
 // omit file extension .json:
@@ -9,10 +9,13 @@ const files_main = [
     "eLTERMetadataSchemaExternalDatasets"
 ]
 
+
 // dereference main files, stringify json result, save as "..._monolith.json":
-await Promise.all(
+Promise.all(
     files_main.map(async (stub_filename) => {
-        let outfile_name = `${stub_filename}_monolith_test.json`        
+        let outfile_name = path.join(
+            __dirname, `${stub_filename}_monolith.json`
+        )        
         let o_json = await $RefParser.dereference(`${stub_filename}.json`)
         fs.writeFile(outfile_name,
             JSON.stringify(o_json, null, 2),
@@ -20,6 +23,3 @@ await Promise.all(
         )        
     })
 );
-
-
-
