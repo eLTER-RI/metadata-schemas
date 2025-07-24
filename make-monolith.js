9de9c299f30fb2b7275e80b4300b0241ead29db0
IO <ivo.offenthaler@gmail.com>
2025-07-24 13:52:02 +0200

    add gh workflow and scripts to make monolith

---
+++ make-monolith.js
@@ -0,0 +1,25 @@
+const { default: $RefParser } = await import("@apidevtools/json-schema-ref-parser");
+const fs = await import("fs")
+
+
+// dereference these files by inlining referenced content (subschemas)
+// omit file extension .json:
+const files_main = [
+    "eLTERMetadataSchemaDatasets",
+    "eLTERMetadataSchemaExternalDatasets"
+]
+
+// dereference main files, stringify json result, save as "..._monolith.json":
+await Promise.all(
+    files_main.map(async (stub_filename) => {
+        let outfile_name = `${stub_filename}_monolith.json`        
+        let o_json = await $RefParser.dereference(`${stub_filename}.json`)
+        fs.writeFile(outfile_name,
+            JSON.stringify(o_json, null, 2),
+            (error) => {if(error){console.error(error)}}
+        )        
+    })
+);
+
+
+
